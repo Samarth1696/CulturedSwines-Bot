12acda6abc9c6ed383dce7e729f428ee39fed23d
Samarth1696 <samarthrobo03@gmail.com>
2022-04-02 20:00:05 +0530

    First commit

---
+++ src/main.rs
@@ -0,0 +1,65 @@
+use serenity::{
+    async_trait,
+    model::{channel::Message, gateway::Ready},
+    prelude::*,
+};
+
+const HELP_MESSAGE: &str = "
+Hello there, Human!
+
+You have summoned me. Let's see about getting you what you need.
+
+❓ Need technical help?
+➡️ Post in the <#959714974274560024> channel and other humans will assist you.
+
+❓ Looking for the Code of Conduct?
+➡️ Here it is: <https://opensource.facebook.com/code-of-conduct>
+
+❓ Something wrong?
+➡️ You can flag an admin with @admin
+
+I hope that resolves your issue!
+
+— HelpBot 🤖
+";
+
+const HELP_COMMAND: &str = "!help";
+
+struct Handler;
+
+#[async_trait]
+impl EventHandler for Handler {
+    async fn message(&self, ctx: Context, msg: Message) {
+
+    if msg.author.id == 740203174836830229 {
+            if let Err(why) = msg.channel_id.say(&ctx.http, "<@740203174836830229> https://media.giphy.com/media/UThvDeKMTfsj67t7nG/giphy-downsized-large.gif").await {
+                println!("Gawar Developer.. mera code ekbar check karle.. iss suvar ko msg nahi kar pa raha hu! : {:?}", why);
+
+            }
+        }
+
+        if msg.content == HELP_COMMAND {
+            if let Err(why) = msg.channel_id.say(&ctx.http, HELP_MESSAGE).await {
+                println!("Error sending message: {:?}", why);
+            }
+        }
+    }
+
+    async fn ready(&self, _: Context, ready: Ready) {
+        println!("{} is connected!", ready.user.name);
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    let token = "OTU5NzA0NzQyNzI3MTI3MTAw.YkfwsQ.iXdhCl4bLncGwWzUAFj1Uv1XF80";
+
+    let mut client = Client::new(&token)
+        .event_handler(Handler)
+        .await
+        .expect("Err creating client");
+
+    if let Err(why) = client.start().await {
+        println!("Client error: {:?}", why);
+    }
+}
