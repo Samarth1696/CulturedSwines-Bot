64e1a02ab1d93478a993f14780010a25b319d313
Samarth1696 <samarthrobo03@gmail.com>
2023-06-26 02:12:15 +0530

    Modified main with the previous code

--- src/main.rs
+++ src/main.rs
@@ -8,12 +8,41 @@ use tracing::{error, info};
 
 struct Bot;
 
+const HELP_MESSAGE: &str = "
+Hello there, Human!
+
+You have summoned me. Let's see about getting you what you need.
+
+❓ Need technical help?
+➡️ Post in the <#959714974274560024> channel and other humans will assist you.
+
+❓ Looking for the Code of Conduct?
+➡️ Here it is: <https://opensource.facebook.com/code-of-conduct>
+
+❓ Something wrong?
+➡️ You can flag an admin with @admin
+
+I hope that resolves your issue!
+
+— HelpBot 🤖
+";
+
+const HELP_COMMAND: &str = "!help";
+
 #[async_trait]
 impl EventHandler for Bot {
     async fn message(&self, ctx: Context, msg: Message) {
-        if msg.content == "!hello" {
-            if let Err(e) = msg.channel_id.say(&ctx.http, "world!").await {
-                error!("Error sending message: {:?}", e);
+
+        if msg.author.id == 740203174836830229 {
+            if let Err(why) = msg.channel_id.say(&ctx.http, "<@740203174836830229> https://media.giphy.com/media/UThvDeKMTfsj67t7nG/giphy-downsized-large.gif").await {
+                println!("Gawar Developer.. mera code ekbar check karle.. iss suvar ko msg nahi kar pa raha hu! : {:?}", why);
+
+            }
+        }
+
+        if msg.content == HELP_COMMAND {
+            if let Err(why) = msg.channel_id.say(&ctx.http, HELP_MESSAGE).await {
+                println!("Error sending message: {:?}", why);
             }
         }
     }
